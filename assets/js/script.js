07ab91c428fe2ead47a553c03d27ef71b18985aa
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2022-08-09 13:56:07 +0000

    add data to js

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const quizQuestion = [
+     {
+        question: 'Which football club plays its home games at Loftus Road?',
+        a: 'Manchester United',
+        b: 'Queens Park Rangers',
+        c: 'Wolverhamton Wanderers',
+        d: 'Luton Town',
+        correct: 'b'
+     },{
+        question: 'Name the coffee shop in the US Sitcom Friends.',
+        a: 'The Big Tea',
+        b: 'Coders Coffehouse',
+        C: 'Beanhive',
+        d: 'Central Perk',
+        correct: 'd'
+     },{
+        question: 'From what grain is the Japenese spirit Sake made?',
+        a: 'Wheat',
+        b: 'Rye',
+        c: 'Rice',
+        d: 'Barley',
+        correct: 'c'
+     }, {
+        question: 'What is rapper P Diddys real name?',
+        a: 'Willy Wonka',
+        b: 'Bruce Forsyth',
+        c: 'Sean Combs',
+        d: 'Marshal Mathers',
+        correct: 'c',
+     }
+
+
+]
+
